bde668c48ca206a6a580e0e0cd8860fcedba3009
lupoj <lupoj@wit.edu>
2026-07-15 12:48:46 -0400

    Added code to part a

--- parta.c
+++ parta.c
@@ -1,8 +1,14 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
+    char text[100];
+
+    printf("Enter text: ");
+    fgets(text, sizeof(text), stdin);
+    printf(text);
     return 0;
 }
