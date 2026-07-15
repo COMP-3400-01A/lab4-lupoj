d939df878457d243fe642e901c7aedd5003387ab
lupoj <lupoj@wit.edu>
2026-07-15 16:18:29 -0400

    Code is fully functional

--- parta.c
+++ parta.c
@@ -1,14 +1,34 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
     char text[100];
 
-    printf("Enter text: ");
-    fgets(text, sizeof(text), stdin);
-    printf(text);
+    if (argc > 1) {
+        strncpy(text, argv[1], sizeof(text) - 1);
+        text[sizeof(text) - 1] = '\0';
+
+    }
+    else {
+        printf("Enter: ");
+        fgets(text, sizeof(text), stdin);
+        text[strcspn(text, "\n")] = '\0';
+    }
+
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (islower(text[i])) {
+            text[i] = toupper(text[i]);
+        }
+        else if (isspace(text[i])) {
+            text[i] = ',';
+        }
+    }
+
+    printf("%s\n", text);
     return 0;
 }
