58d05a84f96901a367a0eab0b46e8a14fd65f0d6
lupoj <lupoj@wit.edu>
2026-07-15 16:35:08 -0400

    Added documentation

--- partc.c
+++ partc.c
@@ -9,13 +9,20 @@
 
 int main(int argc, const char* argv[]) {
     // TODO: Complete and document
+    // If number of arguments is not 3, then insufficent or invalid arguments have 
+    // been supplied by the user and exit with status of 1.
     if (argc != 3) {
         write(2, "ERROR: no arguments\n", 20);
         return 1;
     }
 
+    // Initialize pid for the fork
     pid_t pid = fork();
 
+    // If the process ID is less than 0, that indicates that an error has occurred
+    // So exit with status of 1
+    // If the PID is 0, do child process
+    // Otherwise, do the parent process
     if (pid < 0) {
         perror("fork failed");
         return 1;
